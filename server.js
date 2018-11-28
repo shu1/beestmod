801445e8aa60cb5837d11149d07a3096f3aa2a55
Shuichi Aizawa <sa396@columbia.edu>
2018-11-28 03:34:57 -0500

    init, code cleanup

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
 // Shuichi Aizawa 2018 github.com/shu1
 "use strict";
 
+var https = require("https");
 var express = require("express");
 var app = express();
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("./.data/sqlite.db");
 
-db.run("CREATE TABLE IF NOT EXISTS alphavantage(datetime TEXT NOT NULL, function TEXT NOT NULL, symbol TEXT NOT NULL, json TEXT NOT NULL)", function(err) {
-	err && console.error(err);
+db.run("CREATE TABLE alphavantage(datetime TEXT NOT NULL, function TEXT NOT NULL, symbol TEXT NOT NULL, json TEXT NOT NULL)", function(err) {
+	if (!err) {
+		console.error("table created");
+		init("TIME_SERIES_DAILY_ADJUSTED", ["FB","AAPL","AMZN","NFLX","GOOG"]);
+		setTimeout(init, 1000*65,  "DIGITAL_CURRENCY_DAILY", ["BTC","BCH","ETH","EOS","XLM"]);
+		setTimeout(init, 1000*130, "DIGITAL_CURRENCY_DAILY", ["XMR","DASH"]);
+	}
 });
+
+function init(f, s) {
+	for (var i=0; i<5 && i<s.length; ++i) {
+		get(f, s[i]);
+	}
+}
+
+function get(f, s, update, res) {
+	https.get("https://www.alphavantage.co/query?function=" + f + "&symbol=" + s + "&market=USD&apikey=" + process.env.apikey, function(response) {
+		var data = "";
+		response.on("data", function(chunk) {data += chunk});
+		response.on("end", function() {
+			var parsed = JSON.parse(data);
+			if (parsed["Meta Data"]) {
+				if (update) {
+					var query = "UPDATE alphavantage SET datetime = datetime('now'), json = ? WHERE function = ? AND symbol = ?";
+					console.log(s + " update");
+				} else {
+					var query = "INSERT INTO alphavantage(datetime, json, function, symbol) VALUES(datetime('now'), ?, ?, ?)";
+					console.log(s + " insert");
+				}
+
+				db.run(query, [data,f,s], function(err) {
+					err && console.error(err);
+				});
+			} else {
+				console.error(s + " denied");
+				console.log(parsed);
+			}
+			res && res.send(parsed);
+		});
+	});
+}
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
@@ -31,6 +70,26 @@ app.get("/crons", function(req, res) {
 app.get("/cronc", function(req, res) {
 	cron("DIGITAL_CURRENCY_DAILY", 10000000, res);
 });
+
+function cron(f, prev, res) {
+	console.log("cron " + f + " " + prev);
+	db.all("SELECT function, symbol FROM alphavantage WHERE function = ? AND date(datetime) < date('now') ORDER BY datetime", [f], function(err, rows) {
+		if (err) {
+			console.error(err);
+			res && res.send(err);
+		} else {
+			for (var i=0; i<5 && i<rows.length; ++i) {
+				console.log("cron " + rows[i].symbol);
+				get(rows[i].function, rows[i].symbol, true);
+			}
+			res && res.send(rows);
+
+			if (rows.length > 0 && rows.length < prev) {
+				setTimeout(cron, 1000*65, f, rows.length);
+			}
+		}
+	});
+}
 
 app.get("/query", function(req, res) {
 	db.get("SELECT * FROM alphavantage WHERE function = ? AND symbol = ?", [req.query.function, req.query.symbol], function(err, row) {
@@ -63,54 +122,6 @@ app.get("/query", function(req, res) {
 		}
 	});
 });
-
-function cron(f, prev, res) {
-	console.log("cron " + f + " " + prev);
-	db.all("SELECT function, symbol FROM alphavantage WHERE function = ? AND date(datetime) < date('now') ORDER BY datetime", [f], function(err, rows) {
-		if (err) {
-			console.error(err);
-			res && res.send(err);
-		} else {
-			for (var i=0; i<5 && i<rows.length; ++i) {
-				console.log("cron " + rows[i].symbol);
-				get(rows[i].function, rows[i].symbol, true);
-			}
-			res && res.send(rows);
-
-			if (rows.length > 0 && rows.length < prev) {
-				setTimeout(cron, 1000*60*2, f, rows.length);
-			}
-		}
-	});
-}
-
-function get(f, s, update, res) {
-	var https = require("https");
-	https.get("https://www.alphavantage.co/query?function=" + f + "&symbol=" + s + "&market=USD&apikey=" + process.env.apikey, function(response) {
-		var data = "";
-		response.on("data", function(chunk) {data += chunk});
-		response.on("end", function() {
-			var parsed = JSON.parse(data);
-			if (parsed["Meta Data"]) {
-				if (update) {
-					var query = "UPDATE alphavantage SET datetime = datetime('now'), json = ? WHERE function = ? AND symbol = ?";
-					console.log(s + " update");
-				} else {
-					var query = "INSERT INTO alphavantage(datetime, json, function, symbol) VALUES(datetime('now'), ?, ?, ?)";
-					console.log(s + " insert");
-				}
-
-				db.run(query, [data,f,s], function(err) {
-					err && console.error(err);
-				});
-			} else {
-				console.error(s + " denied");
-				console.log(parsed);
-			}
-			res && res.send(parsed);
-		});
-	});
-}
 
 var listener = app.listen(process.env.PORT, function() {
 	console.log("listening on " + listener.address().port);
