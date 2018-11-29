594e91b1fc4d0bba0b130bcb0600554148a10722
Shuichi Aizawa <sa396@columbia.edu>
2018-11-28 20:42:13 -0500

    upsert

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var app = express();
 var {Pool} = require("pg");
 var pool = new Pool({connectionString:process.env.DATABASE_URL, ssl:true});
 
-pool.query("CREATE TABLE alphavantage(datetime TIMESTAMPTZ NOT NULL, function TEXT NOT NULL, symbol TEXT NOT NULL, json TEXT NOT NULL)", function(err, result) {
-	if (!err) {
-		console.error("table created");
+pool.query("CREATE TABLE alphavantage(datetime TIMESTAMPTZ NOT NULL, function TEXT NOT NULL, symbol TEXT NOT NULL, data TEXT NOT NULL, PRIMARY KEY (function, symbol))", function(err, result) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.error("TABLE CREATED");
 		init("TIME_SERIES_DAILY_ADJUSTED", ["FB","AAPL","AMZN","NFLX","GOOG"]);
 		setTimeout(init, 1000*65,  "DIGITAL_CURRENCY_DAILY", ["BTC","BCH","ETH","EOS","XLM"]);
 		setTimeout(init, 1000*130, "DIGITAL_CURRENCY_DAILY", ["XMR","DASH"]);
@@ -22,27 +24,19 @@ function init(f, s) {
 	}
 }
 
-function get(f, s, update, res) {
+function get(f, s, res) {
 	https.get("https://www.alphavantage.co/query?function=" + f + "&symbol=" + s + "&market=USD&apikey=" + process.env.apikey, function(response) {
 		var data = "";
 		response.on("data", function(chunk) {data += chunk});
 		response.on("end", function() {
 			var parsed = JSON.parse(data);
 			if (parsed["Meta Data"]) {
-				if (update) {
-					var query = "UPDATE alphavantage SET datetime = NOW(), json = $1 WHERE function = $2 AND symbol = $3";
-					console.log(s + " update");
-				} else {
-					var query = "INSERT INTO alphavantage(datetime, json, function, symbol) VALUES(NOW(), $1, $2, $3)";
-					console.log(s + " insert");
-				}
-
-				pool.query(query, [data,f,s], function(err, result) {
+				console.log(s + " insert");
+				pool.query("INSERT INTO alphavantage(datetime, function, symbol, data) VALUES(NOW(), $1, $2, $3) ON CONFLICT(function, symbol) DO UPDATE SET datetime = NOW(), data = $3", [f,s,data], function(err, result) {
 					err && console.error(err);
 				});
 			} else {
 				console.error(s + " denied");
-				console.log(parsed);
 			}
 			res && res.send(parsed);
 		});
@@ -73,18 +67,17 @@ app.get("/cronc", function(req, res) {
 
 function cron(f, prev, res) {
 	console.log("cron " + f + " " + prev);
-	pool.query("SELECT function, symbol FROM alphavantage WHERE function = $1 AND date_trunc('day', datetime) < CURRENT_DATE ORDER BY datetime", [f], function(err, result) {
+	pool.query("SELECT function, symbol FROM alphavantage WHERE function = $1", [f], function(err, result) {
 		if (err) {
 			console.error(err);
 			res && res.send(err);
 		} else {
 			for (var i=0; i<5 && i<result.rowCount; ++i) {
-				console.log("cron " + result.rows[i].symbol);
-				get(result.rows[i].function, result.rows[i].symbol, true);
+				get(result.rows[i].function, result.rows[i].symbol);
 			}
 			res && res.send(result);
 
-			if (result.rowCount > 0 && result.rowCount < prev) {
+			if (result.rowCount > 5 && result.rowCount < prev) {
 				setTimeout(cron, 1000*65, f, result.rowCount);
 			}
 		}
@@ -92,33 +85,16 @@ function cron(f, prev, res) {
 }
 
 app.get("/query", function(req, res) {
-	pool.query("SELECT * FROM alphavantage WHERE function = $1 AND symbol = $2", [req.query.function, req.query.symbol], function(err, result) {
+	pool.query("SELECT data FROM alphavantage WHERE function = $1 AND symbol = $2 AND date_trunc('day', datetime) = CURRENT_DATE", [req.query.function, req.query.symbol], function(err, result) {
 		if (err) {
 			console.error(err);
 		}
 		else if (result && result.rowCount > 0) {
 			var row = result.rows[0];
-			var date = new Date(row.datetime);
-			var now = new Date();	// must be UTC
-			console.log(req.query.symbol + " " + row.datetime);
-
-			if (now.getFullYear() > date.getFullYear()) {
-				row = "update";
-			}
-			else if (now.getMonth() > date.getMonth()) {
-				row = "update";
-			}
-			else if (now.getDate() > date.getDate()) {
-				row = "update";
-			}
-			else {
-				res.send(JSON.parse(row.json));
-			}
-		}
-
-		if (!row || row == "update") {
-			console.log(req.query.symbol + " get");
-			get(req.query.function, req.query.symbol, row == "update", res);
+			console.log(req.query.symbol + " in db");
+			res.send(JSON.parse(row.data));
+		} else {
+			get(req.query.function, req.query.symbol, res);
 		}
 	});
 });
