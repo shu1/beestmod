1a1edac0336bc05ff8ef355a260c0869d6e1c8a8
Shuichi Aizawa <saizawa@gmail.com>
2026-07-16 21:50:51 +0900

    lint
    Apply consistent indentation and spacing throughout index.html and server.js. Changes are formatting only with no functional modifications.

--- server.js
+++ server.js
@@ -7,36 +7,36 @@ var app = express();
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("./.data/sqlite.db");
 
-db.run("CREATE TABLE alphavantage(datetime TEXT NOT NULL, function TEXT NOT NULL, symbol TEXT NOT NULL, data TEXT NOT NULL, PRIMARY KEY(function, symbol))", function(err) {
+db.run("CREATE TABLE alphavantage(datetime TEXT NOT NULL, function TEXT NOT NULL, symbol TEXT NOT NULL, data TEXT NOT NULL, PRIMARY KEY(function, symbol))", function (err) {
 	if (err) {
 		console.log(err);
 	} else {
 		console.warn("table created");
-		init("TIME_SERIES_DAILY_ADJUSTED", ["META","AAPL","AMZN","NFLX","GOOG"]);
-		setTimeout(init, 65000, "DIGITAL_CURRENCY_DAILY", ["BTC","BCH","ETH","EOS","XLM"]);
-		setTimeout(init, 130000, "DIGITAL_CURRENCY_DAILY", ["XMR","DASH","LTC","XRP","ETC"]);
+		init("TIME_SERIES_DAILY_ADJUSTED", ["META", "AAPL", "AMZN", "NFLX", "GOOG"]);
+		setTimeout(init, 65000, "DIGITAL_CURRENCY_DAILY", ["BTC", "BCH", "ETH", "EOS", "XLM"]);
+		setTimeout(init, 130000, "DIGITAL_CURRENCY_DAILY", ["XMR", "DASH", "LTC", "XRP", "ETC"]);
 	}
 })
 
 function init(f, s) {
-	for (var i=0; i<s.length; ++i) {
+	for (var i = 0; i < s.length; ++i) {
 		get(f, s[i]);
 	}
 }
 
-app.get("/get", function(req, res) {
+app.get("/get", function (req, res) {
 	get(req.query.function, req.query.symbol, res);
 })
 
 function get(f, s, res) {
-	https.get("https://www.alphavantage.co/query?function=" + f + "&symbol=" + s + "&market=USD&outputsize=full&apikey=" + process.env.apikey, function(response) {
+	https.get("https://www.alphavantage.co/query?function=" + f + "&symbol=" + s + "&market=USD&outputsize=full&apikey=" + process.env.apikey, function (response) {
 		var data = "";
-		response.on("data", function(chunk) {data += chunk});
-		response.on("end", function() {
+		response.on("data", function (chunk) { data += chunk });
+		response.on("end", function () {
 			var parsed = JSON.parse(data);
 			if (parsed["Meta Data"]) {
 				console.log(s, "insert");
-				db.run("INSERT OR REPLACE INTO alphavantage(datetime, function, symbol, data) VALUES(datetime('now'), ?, ?, ?)", [f,s,data], function(err) {
+				db.run("INSERT OR REPLACE INTO alphavantage(datetime, function, symbol, data) VALUES(datetime('now'), ?, ?, ?)", [f, s, data], function (err) {
 					err && console.error(err);
 				})
 			} else {
@@ -47,7 +47,7 @@ function get(f, s, res) {
 	})
 }
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 	if (!Object.keys(req.query).length) {
 		res.redirect("/?stocks=META,AAPL,AMZN,NFLX,GOOG&crypto=BTC,BCH,ETH,EOS,XLM,XMR,DASH&date=2018-08-13");
 	} else {
@@ -55,8 +55,8 @@ app.get("/", function(req, res) {
 	}
 })
 
-app.get("/all", function(req, res) {
-	db.all("SELECT symbol, datetime FROM alphavantage ORDER BY datetime", function(err, rows) {
+app.get("/all", function (req, res) {
+	db.all("SELECT symbol, datetime FROM alphavantage ORDER BY datetime", function (err, rows) {
 		if (err) {
 			console.error(err);
 			res.status(500).send(err);
@@ -66,8 +66,8 @@ app.get("/all", function(req, res) {
 	})
 })
 
-app.get("/one", function(req, res) {
-	db.get("SELECT * FROM alphavantage WHERE function=? AND symbol=?", [req.query.function, req.query.symbol], function(err, row) {
+app.get("/one", function (req, res) {
+	db.get("SELECT * FROM alphavantage WHERE function=? AND symbol=?", [req.query.function, req.query.symbol], function (err, row) {
 		if (err) {
 			console.error(err);
 			res.status(500).send(err);
@@ -77,8 +77,8 @@ app.get("/one", function(req, res) {
 	})
 })
 
-app.get("/delete", function(req, res) {
-	db.run("DELETE FROM alphavantage WHERE function=? AND symbol=?", [req.query.function, req.query.symbol], function(err) {
+app.get("/delete", function (req, res) {
+	db.run("DELETE FROM alphavantage WHERE function=? AND symbol=?", [req.query.function, req.query.symbol], function (err) {
 		if (err) {
 			console.error(err);
 			res.status(500).send(err);
@@ -89,22 +89,22 @@ app.get("/delete", function(req, res) {
 	})
 })
 
-app.get("/cron", function(req, res) {
+app.get("/cron", function (req, res) {
 	var date = new Date();
 	if (req.query.h && req.query.m) {
 		date.setHours(req.query.h, req.query.m, 0);
 	}
-	cron(req.query.f=="c"?"DIGITAL_CURRENCY_DAILY":"TIME_SERIES_DAILY_ADJUSTED", date.toISOString(), 10000000, res);
+	cron(req.query.f == "c" ? "DIGITAL_CURRENCY_DAILY" : "TIME_SERIES_DAILY_ADJUSTED", date.toISOString(), 10000000, res);
 })
 
 function cron(f, time, prev, res) {
 	console.log("cron", f, prev);
-	db.all("SELECT function, symbol FROM alphavantage WHERE function=? AND datetime(datetime) < datetime(?) ORDER BY datetime", [f,time], function(err, rows) {
+	db.all("SELECT function, symbol FROM alphavantage WHERE function=? AND datetime(datetime) < datetime(?) ORDER BY datetime", [f, time], function (err, rows) {
 		if (err) {
 			console.error(err);
 			res && res.status(500).send(err);
 		} else {
-			for (var i=0; i<5 && i<rows.length; ++i) {
+			for (var i = 0; i < 5 && i < rows.length; ++i) {
 				get(rows[i].function, rows[i].symbol);
 			}
 			res && res.send(rows);
@@ -116,8 +116,8 @@ function cron(f, time, prev, res) {
 	})
 }
 
-app.get("/query", function(req, res) {
-	db.get("SELECT data FROM alphavantage WHERE function=? AND symbol=?", [req.query.function, req.query.symbol], function(err, row) {
+app.get("/query", function (req, res) {
+	db.get("SELECT data FROM alphavantage WHERE function=? AND symbol=?", [req.query.function, req.query.symbol], function (err, row) {
 		if (err) {
 			console.error(err);
 			res && res.status(500).send(err);
@@ -132,6 +132,6 @@ app.get("/query", function(req, res) {
 	})
 })
 
-var listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT, function () {
 	console.log("app is listening on", listener.address().port);
 })
